ab40a5c37b0a45887cd63e2b5bf4638d9cf2917e
Daria <gromova.d15@gmail.com>
2022-10-02 19:48:39 +0300

    initial commit

---
+++ controller.py
@@ -0,0 +1,8 @@
+from sort import sort,get
+from vvod import get_number,getd
+from file import read_file,write_file
+from functional import sortd
+
+
+def button():
+    return sortd(get_number,read_file,write_file,sort,get,getd)
